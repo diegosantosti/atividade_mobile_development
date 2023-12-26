14a5be357b807429c0c184a71197c1c520f0498e
Anderson-Nunes-Souza <anderson.nunes.souza@hotmail.com>
2023-12-25 22:04:05 -0300

    - Adicionado Rolelist.tsx
    - adicionado componente de Rolelist para exibir no cadastro de usuários.

---
+++ src/components/Rolelist.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { SelectList} from "react-native-dropdown-select-list";
+import { Roles } from "../model/roles"
+
+type Props = {
+    role: Roles,
+    onPress: (role: Roles) => void
+}
+
+export default function Rolelist(){
+    const [selected, setSelected] = React.useState("");
+  
+  const data = [
+      {key:'1', value:'Mobiles', disabled:true},
+      {key:'2', value:'Appliances'},
+      {key:'3', value:'Cameras'},
+      {key:'4', value:'Computers', disabled:true},
+      {key:'5', value:'Vegetables'},
+      {key:'6', value:'Diary Products'},
+      {key:'7', value:'Drinks'},
+  ]
+  return(
+    <SelectList 
+        setSelected={(val: React.SetStateAction<string>) => setSelected(val)} 
+        data={data} 
+        save="value"
+    />
+  )
+};
